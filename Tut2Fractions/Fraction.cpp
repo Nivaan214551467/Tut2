8dcff37458aa3d114419f0fbc9ff7747debe99a0
Nivaan214551467 <krishunduttn@gmail.com>
2016-02-27 16:42:44 +0200

    27-02-2016_16:42

--- Tut2Fractions/Fraction.cpp
+++ Tut2Fractions/Fraction.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-int main(){
-
-}
-
 
 class Fraction{
 private:
@@ -15,14 +11,62 @@ private:
 public:
 	Fraction(int,int);
 	
-	int add(Fraction);
+	Fraction getNumDenom();
+	void setNumDenom(int, int);
+	Fraction add(Fraction);
+	Fraction subtract(Fraction);
+	Fraction multiply(Fraction);
+	Fraction divide(Fraction);
+	void print();
+	int hcf(int,int);
 };
 
-Fraction::Fraction(int n = 1, int d = 1){
+Fraction::Fraction(int n, int d){
 	num = n;
 	denom = d;
 }
 
-int Fraction::add(Fraction x){
-	num = num*x.denom;
+Fraction Fraction::getNumDenom(){
+	return Fraction(num,denom);
+}
+
+void Fraction::setNumDenom(int a, int b){
+	num = a;
+	denom = b;
+}
+
+
+Fraction Fraction::add(Fraction x){
+	num = (num*x.denom) + (x.num * denom);
+	denom = denom * x.denom;
+	return Fraction(num,denom);
+}
+
+Fraction Fraction::subtract(Fraction x){
+	num = (num*x.denom) - (x.num * denom);
+	denom = denom * x.denom;
+	return Fraction(num, denom);
+}
+
+Fraction Fraction::multiply(Fraction x){
+	num = num*x.num;
+	denom = denom * x.denom;
+	return Fraction(num, denom);
+}
+Fraction Fraction::divide(Fraction x){
+	num = num * x.denom;
+	denom = denom * x.num;
+	return Fraction(num, denom);
+}
+
+int Fraction::hcf(int x, int y) {
+	if (y == 0)
+		return x;
+	else return
+		hcf(y, x % y);
+}
+
+void Fraction::print()
+{
+	cout << (num / hcf(num, denom)) << "/" << (denom / hcf(num, denom));
 }
