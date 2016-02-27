ebc23a84eb43abfd4f2bac030776b7b5814c46ab
Nivaan214551467 <krishunduttn@gmail.com>
2016-02-27 17:13:12 +0200

    27-02-2016_17:13

--- Tut2Fractions/Fraction.cpp
+++ Tut2Fractions/Fraction.cpp
@@ -11,7 +11,7 @@ private:
 public:
 	Fraction(int,int);
 	
-	Fraction getNumDenom();
+	Fraction getNumDenom();			//Declaration of all methods
 	void setNumDenom(int, int);
 	Fraction add(Fraction);
 	Fraction subtract(Fraction);
@@ -21,52 +21,48 @@ public:
 	int hcf(int,int);
 };
 
-Fraction::Fraction(int n, int d){
+Fraction::Fraction(int n, int d){			//Constructor
 	num = n;
 	denom = d;
 }
 
-Fraction Fraction::getNumDenom(){
+Fraction Fraction::getNumDenom(){			//Returns a Fraction when called
 	return Fraction(num,denom);
 }
 
-void Fraction::setNumDenom(int a, int b){
-	num = a;
-	denom = b;
+void Fraction::setNumDenom(int x, int y){	//Sets the fraction to the values given
+	num = x;
+	denom = y;
 }
 
 
-Fraction Fraction::add(Fraction x){
-	num = (num*x.denom) + (x.num * denom);
-	denom = denom * x.denom;
-	return Fraction(num,denom);
+Fraction Fraction::add(Fraction x){			//Adds the Fraction to another given Fraction
+	return Fraction(((num*x.denom) + (x.num * denom)), (denom * x.denom));
 }
 
-Fraction Fraction::subtract(Fraction x){
-	num = (num*x.denom) - (x.num * denom);
-	denom = denom * x.denom;
-	return Fraction(num, denom);
+Fraction Fraction::subtract(Fraction x){	//Subtracts a given fration from this fraction
+	return Fraction(((num*x.denom) - (x.num * denom)), (denom * x.denom));
 }
 
-Fraction Fraction::multiply(Fraction x){
-	num = num*x.num;
-	denom = denom * x.denom;
-	return Fraction(num, denom);
+Fraction Fraction::multiply(Fraction x){	//Multiplies two fractions
+	return Fraction((num*x.num), (denom * x.denom));
 }
-Fraction Fraction::divide(Fraction x){
-	num = num * x.denom;
-	denom = denom * x.num;
-	return Fraction(num, denom);
+Fraction Fraction::divide(Fraction x){		//Divides two frations
+	return Fraction((num * x.denom), (denom * x.num));
 }
-
-int Fraction::hcf(int x, int y) {
+	
+int Fraction::hcf(int x, int y) {			//Finds the Highest Common Factor between the Numerator and Denominator so simplification can be done
 	if (y == 0)
 		return x;
 	else return
 		hcf(y, x % y);
 }
 
-void Fraction::print()
-{
-	cout << (num / hcf(num, denom)) << "/" << (denom / hcf(num, denom));
+void Fraction::print(){						//prints the fraction in simplest form
+	if (num == 0)
+		cout << "0" << endl;
+	else if ((denom / hcf(num, denom)) == 1)
+		cout << num << endl;
+	else
+	    cout << (num / hcf(num, denom)) << "/" << (denom / hcf(num, denom))<<endl;
 }
